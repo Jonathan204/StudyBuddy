9c21c1b2c62cbd345ed18308bc2e0f5847d2cb56
Jonathan Kuz <43478694+Jonathan204@users.noreply.github.com>
2021-03-20 21:08:28 -0500

    Add the sliders to the metric page
    Add sliders that are adjustable

--- src/components/Account/SelfMetrics.js
+++ src/components/Account/SelfMetrics.js
@@ -2,12 +2,20 @@ import React, { useContext, useState } from "react";
 import { Form, Container, Row, Col } from "react-bootstrap";
 import { RegisterContext } from "./RegisterContext";
 import LoaderButton from "../Button/LoadingButton";
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+import RangeSlider from 'react-bootstrap-range-slider';
 
 const SelfMetrics = () => {
   const { switchToCredentials } = useContext(RegisterContext);
   const { switchToBuddyMetrics } = useContext(RegisterContext);
   const { switchToProfile } = useContext(RegisterContext);
   const { switchToTags } = useContext(RegisterContext);
+
+  const [ chattyValue, setChattyValue ] = useState(0); 
+  const [ seriousValue, setSeriousValue ] = useState(0); 
+  const [ friendlyValue, setFriendlyValue ] = useState(0); 
+  const [ brainyValue, setBrainyValue ] = useState(0); 
+  const [ gradesValue, setGradesValue ] = useState(0); 
 
   return (
     <Container className="register">
@@ -18,13 +26,40 @@ const SelfMetrics = () => {
         <button onClick={switchToProfile}></button>
         <button onClick={switchToTags}></button>
       </Row>
-      <Row className="mt-4 align-text-center">
+      <Row className="mt-4 text-left">
         <Col>
           <h5>Set your metrics.</h5>
           <p>What type of study buddy are you?</p>
+          <div className="mx-3">
+          <h5 className="text-left">Chatty</h5>
+          <RangeSlider
+            value={chattyValue}
+            onChange={changeEvent => setChattyValue(changeEvent.target.value)}
+          />
+          <h5 className="text-left">Serious</h5>
+          <RangeSlider
+            value={seriousValue}
+            onChange={changeEvent => setSeriousValue(changeEvent.target.value)}
+          />          
+          <h5 className="text-left">Friendly</h5>
+          <RangeSlider
+            value={friendlyValue}
+            onChange={changeEvent => setFriendlyValue(changeEvent.target.value)}
+          />
+          <h5 className="text-left">Brainy</h5>
+          <RangeSlider
+            value={brainyValue}
+            onChange={changeEvent => setBrainyValue(changeEvent.target.value)}
+          />   
+          <h5 className="text-left">Focused on grades</h5>
+          <RangeSlider
+            value={gradesValue}
+            onChange={changeEvent => setGradesValue(changeEvent.target.value)}
+          /> 
+          </div>
         </Col>
       </Row>
-      <Row className="mt-5 next-cancel-btn">
+      <Row className="mt-3 next-cancel-btn">
         <LoaderButton className="back-button" onClick={switchToCredentials}>Back</LoaderButton>
         <LoaderButton className="next-button" onClick={switchToBuddyMetrics}>Next</LoaderButton>
       </Row>
