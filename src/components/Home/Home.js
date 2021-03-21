8b17e70d2afbceef29fe52f14f9d0461e1c2d5d7
NathanCarriere <carrie34@myumanitoba.ca>
2021-03-21 11:48:13 -0500

    Home Page Finished, No button functions
    Home Page UI implemented. We may want to play around with button styling a bit more. Button functionality not yet implemented.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Button, Container, Row, Col } from "react-bootstrap";
 
-
 import "./styles.css";
+import "../Account/styles.css"; // just so we update with other changes
 
 const Home = () => {
 
+  //not hooked up yet
   const handleMetricsClicked = async (event) => {
   };
 
@@ -16,7 +17,7 @@ const Home = () => {
     <Container className="border account-window account-height">
       <Row className="align-text-center top-buffer">
         <Col>
-          <h3 className="text-white">Study Buddy</h3>
+          <h2 className="text-white">Study Buddy</h2>
         </Col>
       </Row>
 
@@ -25,12 +26,21 @@ const Home = () => {
           <Button className="btn btn-secondary btn-lg btn-block rounded-pill">
             Metrics
           </Button>
-          <Button className="mt-2 btn btn-primary btn-lg btn-block rounded-pill">
+          <Button className="mt-4 btn btn-primary btn-lg btn-block rounded-pill">
             Matches
           </Button>
         </Col>
       </Row>
 
+      <Row className="mt-auto mb-3 container-fluid" style={{backgroundColor:'#3A506B'}}>
+    
+        <i className="bi bi-house-fill" style={{ color: '#EEF1EF', fontSize: '2em' }}></i>
+        <Col></Col>
+        <Col><i className="bi bi-chat-square-dots-fill ml-auto mr-auto" style={{color: '#EEF1EF', fontSize:'2em', textAlign:'center'}}></i></Col>
+        <Col></Col>
+        <i className="bi bi-gear-fill" style={{ color: '#EEF1EF', fontSize: '2em', textAlign: 'right' }}></i>
+    
+      </Row>
         
       
     </Container>
