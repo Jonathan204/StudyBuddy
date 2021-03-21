9d2143427137cbe77702a4599a4cbd6c502e613d
Jonathan Kuz <43478694+Jonathan204@users.noreply.github.com>
2021-03-21 17:45:01 -0500

    add profile picture package

--- src/components/Account/Profile.js
+++ src/components/Account/Profile.js
@@ -3,11 +3,25 @@ import { Container, Row, Col } from "react-bootstrap";
 import { RegisterContext } from "./RegisterContext";
 import LoaderButton from "../Button/LoadingButton";
 
+import ProfilePicture from "profile-picture";
+import "profile-picture/build/ProfilePicture.css"
+
 const Profile = () => {
   const { switchToCredentials } = useContext(RegisterContext);
   const { switchToBuddyMetrics } = useContext(RegisterContext);
   const { switchToSelfMetrics } = useContext(RegisterContext);
   const { switchToTags } = useContext(RegisterContext);
+
+  const profilePictureRef = React.createRef();
+
+  function handleUpload() {
+    const PP = this.profilePicture.current;
+    const imageData = PP.getData();
+    const file = imageData.file;
+    const imageAsDataURL = PP.getImageAsDataUrl();
+
+    //add here the upload logic...
+  }
 
   return (
     <Container className="register">
@@ -21,6 +35,13 @@ const Profile = () => {
       <Row className="mt-4 align-text-center">
         <Col>
           <h5>Let's set up your profile.</h5>
+          <ProfilePicture
+            ref={profilePictureRef}
+            useHelper={true}
+            debug={true}
+            frameFormat={"circle"}
+          />
+          <button onClick={handleUpload}>Upload</button>
         </Col>
       </Row>
       <Row className="mt-5 next-cancel-btn">
